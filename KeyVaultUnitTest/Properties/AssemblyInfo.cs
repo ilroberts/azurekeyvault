bea5f5788ce60d70ed7ef8e5c24b042868cd28da
iwan roberts <ilroberts@gmail.com>
2017-06-23 20:02:34 +0100

    added encryption unit test

---
+++ KeyVaultUnitTest/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("KeyVaultUnitTest")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("KeyVaultUnitTest")]
+[assembly: AssemblyCopyright("Copyright ©  2017")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("893a2972-9f19-4122-ab02-845bccf30ade")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ KeyVaultUnitTest/UnitTest1.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Security.Cryptography;
+
+using KeyVaultTest1.Helpers;
+
+namespace KeyVaultUnitTest
+{
+    [TestClass]
+    public class EncryptionTest
+    {
+        [TestMethod]
+        public void TestEncrypt()
+        {
+            using (var random = new RNGCryptoServiceProvider())
+            {
+                var key = new byte[16];
+                random.GetBytes(key);
+
+                var testString = "now is the winter of our discontent made glorious summer by this son of york";
+
+                byte[] result = Encryption.EncryptStringToBytes_Aes(testString, key);
+                string decryptedResult = Encryption.DecryptStringFromBytes_Aes(result, key);
+
+                Assert.AreEqual(testString, decryptedResult);
+            }
+        }
+    }
+}
